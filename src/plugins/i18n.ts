eaa84fa9872fd1bcc4df0d9f2f68c5d95d27cfc0
tzuyi <a0988426059@gmail.com>
2024-05-20 17:11:30 +0800

    docs: fr-FR json

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -4,6 +4,7 @@ import zhTW from '@/locales/zh-TW.json';
 import jaJP from '@/locales/ja-JP.json';
 import zhCN from '@/locales/zh-CN.json';
 import koKR from '@/locales/ko-KR.json';
+import frFR from '@/locales/fr-FR.json';
 
 const i18n = createI18n({
   legacy: import.meta.env.MODE === 'test',
@@ -16,6 +17,7 @@ const i18n = createI18n({
     'ja-JP': jaJP,
     'zh-CN': zhCN,
     'ko-KR': koKR,
+    'fr-FR': frFR,
   },
 });
 
