5a7f267275e38939ef1b8a5dbafeac43d0689478
tzuyi <a0988426059@gmail.com>
2024-07-31 17:40:22 +0800

    test: add index menu test case

---
+++ src/__tests__/e2e/index/menu.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect, type Page } from '@playwright/test';
+
+test.describe('menu', () => {
+  const getShared = (page: Page) => {
+    return {
+      file: page.locator('li:has(img[alt="file icon"])'),
+      archive: page.locator('li:has(img[alt="archive icon"])'),
+      trash: page.locator('li:has(img[alt="trash icon"])'),
+    };
+  };
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('should display correct layout', async ({ page }) => {
+    const shared = getShared(page);
+
+    await expect(shared.file).toBeInViewport();
+    await expect(shared.archive).toBeInViewport();
+    await expect(shared.trash).toBeInViewport();
+  });
+
+  test('active menu item should be highlighted', async ({ page }) => {
+    const shared = getShared(page);
+
+    await expect(shared.file).toHaveClass('bg-white');
+    await expect(page.getByRole('heading', { name: /my files/i })).toBeInViewport();
+
+    await shared.archive.click();
+    await expect(shared.file).not.toHaveClass('bg-white');
+    await expect(shared.archive).toHaveClass('bg-white');
+    await expect(page.getByRole('heading', { name: /archived files/i })).toBeInViewport();
+
+    await shared.trash.click();
+    await expect(shared.archive).not.toHaveClass('bg-white');
+    await expect(shared.trash).toHaveClass('bg-white');
+    await expect(page.getByRole('heading', { name: /trash bin/i })).toBeInViewport();
+  });
+});
