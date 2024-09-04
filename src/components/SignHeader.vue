f8ada89a372422a7e5b673db48fa424846ce9535
tzuyi <a0988426059@gmail.com>
2024-09-04 14:32:18 +0800

    refactor: optimize language popup interactive experience

--- src/components/SignHeader.vue
+++ src/components/SignHeader.vue
@@ -2,30 +2,43 @@
 import { ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import SignIcon from '@/components/SignIcon.vue';
+import { sleep } from '@/utils/common';
 import { I18N_MAP } from '@/configs/common';
 
 const isShowLanguageMenu = ref(false);
+const isDisplayLanguageMenu = ref(false);
 const { locale } = useI18n();
 let closeMenuTimer: NodeJS.Timeout | null = null;
+let isClosingMenu = false;
 
 function changeLocale(code: string) {
   locale.value = code;
   localStorage.setItem('pdf-signature-i18n', code);
+  isClosingMenu = true;
   isShowLanguageMenu.value = false;
 }
 
-function openLanguageMenu() {
+async function openLanguageMenu() {
+  if (isClosingMenu) return;
   if (closeMenuTimer) {
     clearTimeout(closeMenuTimer);
     closeMenuTimer = null;
   }
+  isDisplayLanguageMenu.value = true;
+  await sleep(0);
   isShowLanguageMenu.value = true;
 }
 
 function closeLanguageMenu() {
   closeMenuTimer = setTimeout(() => {
     isShowLanguageMenu.value = false;
-  }, 150);
+  }, 300);
+}
+
+function onTransitionEnd(event: TransitionEvent) {
+  if (event.propertyName !== 'transform' || isShowLanguageMenu.value) return;
+  isDisplayLanguageMenu.value = false;
+  isClosingMenu = false;
 }
 </script>
 
@@ -51,25 +64,29 @@ function closeLanguageMenu() {
         />
 
         <div
-          v-show="isShowLanguageMenu"
-          class="absolute -bottom-4 left-1/2 -translate-x-1/2 border-8 border-transparent border-b-white"
+          v-if="isDisplayLanguageMenu"
+          :class="['sign_header_menu_arrow sign_header_transition', { 'opacity-0 scale-y-0': !isShowLanguageMenu }]"
         ></div>
 
         <teleport to="body">
-          <ul
-            :class="['sign_header_language', isShowLanguageMenu ? 'scale-y-100' : 'scale-y-0']"
+          <div
+            v-if="isDisplayLanguageMenu"
+            :class="['sign_header_menu sign_header_transition', { 'scale-y-0 opacity-0': !isShowLanguageMenu }]"
+            @transitionend="onTransitionEnd"
             @mouseover="openLanguageMenu"
             @mouseleave="closeLanguageMenu"
           >
-            <li
-              v-for="(name, language) in I18N_MAP"
-              :key="language"
-              :class="['text-gray-50', { 'text-black bg-primary': locale === language }]"
-              @click="changeLocale(language)"
-            >
-              {{ name }}
-            </li>
-          </ul>
+            <ul class="sign_header_language">
+              <li
+                v-for="(name, language) in I18N_MAP"
+                :key="language"
+                :class="['text-gray-50', { 'text-black bg-primary': locale === language }]"
+                @click="changeLocale(language)"
+              >
+                {{ name }}
+              </li>
+            </ul>
+          </div>
         </teleport>
       </div>
 
@@ -93,29 +110,21 @@ function closeLanguageMenu() {
   &_icon {
     @apply w-6 h-6 md:w-8 md:h-8 active:scale-90;
   }
+  &_menu {
+    @apply fixed h-fit px-2 py-2 bg-white shadow-md rounded-lg top-12 right-3 origin-top md:top-[70px] md:right-5;
+    &_arrow {
+      @apply absolute -bottom-4 left-1/2 -translate-x-1/2 border-8 border-transparent border-b-white origin-bottom;
+      clip-path: inset(0 0 1.5px 0);
+    }
+  }
   &_language {
-    @apply fixed
-    bg-white
-    shadow-md
-    rounded-lg
-    px-3
-    py-2
-    max-h-[50%]
-    overflow-y-auto
-    overflow-x-hidden
-    transition-transform
-    origin-top
-    top-12
-    right-3
-    md:grid
-    md:grid-cols-3
-    md:top-[70px]
-    md:gap-x-6
-    md:gap-y-1
-    md:right-5;
+    @apply max-h-[50dvh] px-2 overflow-y-auto overflow-x-hidden md:grid md:grid-cols-3 md:gap-x-6 md:gap-y-1;
     li {
       @apply px-3 py-2 cursor-pointer rounded hover:bg-primary hover:text-black transition-colors;
     }
+  }
+  &_transition {
+    @apply transition-[transform_opacity] duration-300;
   }
 }
 </style>
