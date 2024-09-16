1f945295a10fe877aa20cc76650c933bafd9d568
tzuyi <a0988426059@gmail.com>
2024-09-16 16:03:13 +0800

    refactor: preload plugin

--- src/plugins/preload.ts
+++ src/plugins/preload.ts
@@ -3,16 +3,24 @@ export default {
     const genJyuuGothicFont = new FontFace('Gen Jyuu Gothic P', 'url(/font/GenJyuuGothic-P.woff2)');
     const preloads = [genJyuuGothicFont.load()];
 
-    Promise.all(preloads).then(([loadedFont]) => {
+    Promise.all(preloads).then(([genJyuuGothic]) => {
       // loadedFont is now available
-      document.fonts.add(loadedFont);
-      document.body.style.fontFamily = 'Gen Jyuu Gothic P';
+      setupFont(genJyuuGothic, 'Gen Jyuu Gothic P');
 
       // close loading screen
+      closeLoadingScreen();
+    });
+
+    function setupFont(font: FontFace, name: string) {
+      document.fonts.add(font);
+      document.body.style.fontFamily = name;
+    }
+
+    function closeLoadingScreen() {
       const loadingScreen = document.getElementById('loading-screen');
 
       if (!loadingScreen) return;
       loadingScreen.style.display = 'none';
-    });
+    }
   },
 };
