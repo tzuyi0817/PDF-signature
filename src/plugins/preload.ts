0c092edf5300dd8cdfb6aff8309f49b7a4681513
tzuyi <a0988426059@gmail.com>
2024-09-16 15:57:58 +0800

    feat: loading screen

--- src/plugins/preload.ts
+++ src/plugins/preload.ts
@@ -1,10 +1,18 @@
 export default {
   install() {
     const genJyuuGothicFont = new FontFace('Gen Jyuu Gothic P', 'url(/font/GenJyuuGothic-P.woff2)');
+    const preloads = [genJyuuGothicFont.load()];
 
-    genJyuuGothicFont.load().then(loadedFont => {
+    Promise.all(preloads).then(([loadedFont]) => {
+      // loadedFont is now available
       document.fonts.add(loadedFont);
       document.body.style.fontFamily = 'Gen Jyuu Gothic P';
+
+      // close loading screen
+      const loadingScreen = document.getElementById('loading-screen');
+
+      if (!loadingScreen) return;
+      loadingScreen.style.display = 'none';
     });
   },
 };
