7e5a52274bebc612e2878c6dd400fbb8e573cf09
tzuyi <a0988426059@gmail.com>
2025-03-24 16:45:04 +0800

    feat: adjust tailwind theme color settings

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').RequiredOptions} */
+
+export default {
+  singleQuote: true,
+  semi: true,
+  endOfLine: 'lf',
+  printWidth: 120,
+  proseWrap: 'preserve',
+  arrowParens: 'avoid',
+  singleAttributePerLine: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import 'virtual:svg-icons-register';
 
 import '@/style/index.css';
-import '@/style/tailwind.css';
 import App from '@/App.vue';
 import router from '@/router';
 import errorHandlerPlugin from '@/plugins/error-handler';
