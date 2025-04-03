5bc80286f604371fc774bee82a5b5175de932186
tzuyi <a0988426059@gmail.com>
2025-04-03 14:33:32 +0800

    feat: adjust popup style

--- src/components/SignPopup.vue
+++ src/components/SignPopup.vue
@@ -14,10 +14,10 @@ onMounted(() => emit('childMounted'));
 <template>
   <teleport to="body">
     <div
-      class="sign_popup mask"
+      class="sign-popup mask"
       @click.self="$emit('closePopup')"
     >
-      <div class="sign_popup_box">
+      <div class="sign-popup-box">
         <h5 class="border-b-2 border-primary py-1 px-4 text-center font-medium md:pb-4">
           {{ title }}
         </h5>
@@ -27,24 +27,37 @@ onMounted(() => emit('childMounted'));
   </teleport>
 </template>
 
-<style lang="postcss" scoped>
-.sign_popup {
-  @apply bg-black/50 flex items-center justify-center z-[2];
-  &_box {
-    @apply w-[90%]
-    max-w-[530px]
-    min-h-[261px]
-    px-8
-    pt-4
-    pb-7
-    shadow-[0px_4px_15px_rgba(0,0,0,0.2),inset_0px_2px_5px_rgba(135,135,135,0.2),inset_0px_3px_10px_rgba(215,215,215,0.2),inset_0px_-1px_4px_rgba(135,135,135,0.6),inset_0px_-6px_6px_rgba(215,215,215,0.4)]
-    rounded-[40px]
-    flex
-    flex-col
-    justify-between
-    bg-white
-    md:min-h-[335px]
-    md:max-h-[566px];
+<style lang="css" scoped>
+.sign-popup {
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 2;
+}
+
+.sign-popup-box {
+  width: 90%;
+  max-width: 530px;
+  min-height: 261px;
+  padding: 16px 32px 28px;
+  box-shadow:
+    0px 4px 15px rgba(0, 0, 0, 0.2),
+    inset 0px 2px 5px rgba(135, 135, 135, 0.2),
+    inset 0px 3px 10px rgba(215, 215, 215, 0.2),
+    inset 0px -1px 4px rgba(135, 135, 135, 0.6),
+    inset 0px -6px 6px rgba(215, 215, 215, 0.4);
+  border-radius: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: white;
+}
+
+@media (min-width: 768px) {
+  .sign-popup-box {
+    min-height: 335px;
+    max-height: 566px;
   }
 }
 </style>
