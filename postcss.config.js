06831c5015f094f20c2eeef7a4fd4c3064110f24
tzuyi <a0988426059@gmail.com>
2025-04-18 17:07:56 +0800

    feat:  add `autoprefixer` to automatically add prefixes suitable for each browser

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
-import postcssPxtorem from 'postcss-pxtorem';
+import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
+import postcssPxtorem from 'postcss-pxtorem';
 
 export default {
   plugins: [
+    autoprefixer(),
     postcssPxtorem({
       rootValue: 16,
       propList: ['*'],
